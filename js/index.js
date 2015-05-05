1fdcd4efe0e0623b2dba9403136d879d1a606a6c
apeeyush <peeyushagarwal1994@gmail.com>
2015-05-06 02:09:00 +0530

    Send log (to localhost) on emulation; Add stop emulation
    Sends logs to http://localhost:3000/api/logs every 5 sec. for users.
    Also, add a button to stop emulation

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 $(function() {
-  $(".js-submit-query").click(function(){
+  var refreshIntervalIds = []
+  $(".js-start-emulation").click(function(){
     var numUsers = $("#numUsers").val();
     $("#activity-log-container").empty();
     var divSize = 12/numUsers;
@@ -12,18 +13,35 @@ $(function() {
           "<h4>"+
             "User "+(i+1)+" Activity"+
           "</h4>"+
+          "<ul></ul>"+
         "</div>"
       )
       // Start random activity for the user
-      activityGenerator(i);
+      var refreshIntervalId = activityGenerator(i);
+      refreshIntervalIds.push(refreshIntervalId)
     }
 
-  function activityGenerator(numUser){
-    setInterval(function(){
-      var userId = "#user-"+numUser;
-      $(userId).append("<p>Hello</p>");
-    }, 5000);
-  }
-
+    function activityGenerator(numUser){
+      var refreshIntervalId = setInterval(function(){
+        var userId = "#user-"+numUser;
+        var d = new Date();
+        var time = d.getTime();
+        var data = {"session":"demo_session","username":userId,"time":time,"application":"DemoApp","activity": "DemoActivity","event":"Testing API","parameters":{"parameter1":"value1"} , "extrakey1":"extravalue1"};
+        var request = new XMLHttpRequest();
+        request.open('POST', 'http://localhost:3000/api/logs', true);
+        request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+        request.send(JSON.stringify(data));
+        $(userId+"> ul").append("<li>"+JSON.stringify(data, null, ' ')+"</li>");
+      }, 5000);
+      return refreshIntervalId;
+    }
   });
+
+  $(".js-stop-emulation").click(function(){
+    for(var i=0; i<refreshIntervalIds.length; i++){
+      clearInterval(refreshIntervalIds[i]);
+    }
+    refreshIntervalIds = []
+  });
+
 });
