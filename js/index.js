0ae9abc4be30e9bb0779cb50642191827ca3ccbc
apeeyush <peeyushagarwal1994@gmail.com>
2015-05-09 14:40:49 +0530

    Add setTimeout to generate user activities at different times

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 $(function() {
   var refreshIntervalIds = []
+  var timeoutIds = []
   $(".js-start-emulation").click(function(){
     $('.js-stop-emulation').prop('disabled', false);
     $(".js-start-emulation").prop('disabled', true);
@@ -19,23 +20,25 @@ $(function() {
         "</div>"
       )
       // Start random activity for the user
-      var refreshIntervalId = activityGenerator(i);
-      refreshIntervalIds.push(refreshIntervalId)
+      activityGenerator(i);
     }
 
     function activityGenerator(numUser){
-      var refreshIntervalId = setInterval(function(){
-        var userId = "#user-"+numUser;
-        var d = new Date();
-        var time = d.getTime();
-        var data = {"session":"demo_session","username":userId,"time":time,"application":"DemoApp","activity": "DemoActivity","event":"Testing API","parameters":{"parameter1":"value1"} , "extrakey1":"extravalue1"};
-        var request = new XMLHttpRequest();
-        request.open('POST', 'http://localhost:3000/api/logs', true);
-        request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-        request.send(JSON.stringify(data));
-        $(userId+"> ul").append("<li>"+JSON.stringify(data, null, ' ')+"</li>");
-      }, 5000);
-      return refreshIntervalId;
+      var timeoutId = setTimeout(function(){
+        var refreshIntervalId = setInterval(function(){
+          var userId = "#user-"+numUser;
+          var d = new Date();
+          var time = d.getTime();
+          var data = {"session":"demo_session","username":userId,"time":time,"application":"DemoApp","activity": "DemoActivity","event":"Testing API","parameters":{"parameter1":"value1"} , "extrakey1":"extravalue1"};
+          var request = new XMLHttpRequest();
+          request.open('POST', 'http://localhost:3000/api/logs', true);
+          request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+          request.send(JSON.stringify(data));
+          $(userId+"> ul").append("<li>"+JSON.stringify(data, null, ' ')+"</li>");
+        }, 5000);
+        refreshIntervalIds.push(refreshIntervalId);
+      }, numUser*1000);
+      timeoutIds.push(timeoutId)
     }
   });
 
@@ -45,7 +48,11 @@ $(function() {
     for(var i=0; i<refreshIntervalIds.length; i++){
       clearInterval(refreshIntervalIds[i]);
     }
+    for(var i=0; i<timeoutIds.length; i++){
+      clearTimeout(timeoutIds[i]);
+    }
     refreshIntervalIds = []
+    timeoutIds = []
   });
 
 });
